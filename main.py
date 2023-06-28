0ecffc7da1148deef3260645d9ea7ad559f1d04a
Oscar <oscarchiagoziem@gmail.com>
2023-06-28 21:05:07 +0100

    created main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+bot_token = "MTEyMzU0NjI1MTE1NDE2NTc4MQ.Gm3t5I.904RLZSM4CapEPcRqMa2EPpgdR2b9L_a-UkKqo"
+
+generated_url = "https://discord.com/api/oauth2/authorize?client_id=1123546251154165781&permissions=8&scope=bot"
